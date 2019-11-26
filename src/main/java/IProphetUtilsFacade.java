8023630f6faa13eeedf0b09d6ed360ae636e1d52
Jan Svacina <jan@svacina.net>
2019-11-26 16:16:26 -0600

    utils facade

---
+++ src/main/java/IProphetUtilsFacade.java
@@ -0,0 +1,12 @@
+import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.BoundedContext;
+
+public interface IProphetUtilsFacade {
+
+    static void getMsModelSource(){}
+
+
+    void getMsModelByte();
+
+
+    BoundedContext getBoundedContext();
+}
---
+++ src/main/java/ProphetUtilsFacade.java
@@ -0,0 +1,33 @@
+import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.BoundedContext;
+
+public class ProphetUtilsFacade {
+
+    /**
+     * ToDo: change return object to MsModel
+     * @param path - file system path to micro services
+     */
+    public static void getMsModelSource(String path){
+
+    }
+
+
+    /**
+     * ToDo: change return object to MsModel
+     * @param path - file system path to micro services
+     */
+    public static void getMsModelByte(String path) {
+
+    }
+
+
+    /**
+     * Get Data via Jparser, Create SystemContext, pass SystemContext to bounded-context lib
+     * @param path
+     * @return BoundedContext
+     */
+    public static BoundedContext getBoundedContext(String path) {
+        return null;
+    }
+
+
+}
