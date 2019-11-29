406e7d0af50ce71e8a532ce77d1442840696b998
Jan Svacina <jan@svacina.net>
2019-11-29 15:13:52 -0600

    persisting system context for testing

---
+++ src/main/java/filemanager/FileManager.java
@@ -0,0 +1,23 @@
+package filemanager;
+
+import com.google.gson.Gson;
+import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.SystemContext;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileManager {
+
+    public static void writeToFile(SystemContext systemContext){
+        try {
+            Gson gson = new Gson();
+            String json = gson.toJson(systemContext);
+            BufferedWriter writer = new BufferedWriter(new FileWriter("systemContext.json"));
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
