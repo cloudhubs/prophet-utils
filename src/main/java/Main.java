e526f7899d069b8d684c6ddf3631b1e5bb996146
Jan Svacina <jan@svacina.net>
2019-11-29 15:15:21 -0600

    bounded context test method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,18 @@
+import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.BoundedContext;
 import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.SystemContext;
-import systemcontext.SystemContextParser;
+import systemcontext.ProphetUtilsFacade;
 
 public class Main {
     public static void main(String[] args) {
         //Nobody sees this
         String rootPath = "/Users/svacina/git/c2advseproject";
-        String[] microServicePaths = new String[5];
+        String[] microServicePaths = new String[4];
         microServicePaths[0] = "/Users/svacina/git/c2advseproject/cms";
         microServicePaths[1] = "/Users/svacina/git/c2advseproject/qms-backend";
         microServicePaths[2] = "/Users/svacina/git/c2advseproject/ems";
         microServicePaths[3] = "/Users/svacina/git/c2advseproject/user-management";
-        microServicePaths[4] = "/Users/svacina/git/c2advseproject/user-authentication";
-        SystemContext systemContext = ProphetUtilsFacade.getSystemContext(rootPath, microServicePaths);
+//        microServicePaths[3] = "/Users/svacina/git/c2advseproject/user-authentication";
+        BoundedContext systemContext = ProphetUtilsFacade.getBoundedContext(rootPath, microServicePaths);
         System.out.println(systemContext.getSystemName());
     }
 }
