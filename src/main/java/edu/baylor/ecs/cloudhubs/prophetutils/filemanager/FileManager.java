133c9aa6f48fee248baa0f5ca58cdd80ca8d0d24
Jan Svacina <jan@svacina.net>
2019-11-30 18:23:02 -0600

    generating string list from html template

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetutils/filemanager/FileManager.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetutils/filemanager/FileManager.java
@@ -1,11 +1,12 @@
 package edu.baylor.ecs.cloudhubs.prophetutils.filemanager;
 
 import com.google.gson.Gson;
+import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.BoundedContext;
 import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.SystemContext;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileManager {
 
@@ -20,4 +21,38 @@ public class FileManager {
             e.printStackTrace();
         }
     }
+
+    public static String readHtmlTemplates(String path){
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String currentLine = reader.readLine();
+            while (currentLine != null) {
+                builder.append(currentLine);
+                currentLine = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+    public static List<String> readHtmlTemplatesToList(String path){
+        List<String> list = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String currentLine = reader.readLine();
+            while (currentLine != null) {
+                list.add(currentLine);
+                currentLine = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
 }
