f8c38ab6f37be53707695bf27084ecaeba3ff953
Jan Svacina <jan@svacina.net>
2019-11-30 18:24:03 -0600

    utils tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.BoundedContext;
 import edu.baylor.ecs.cloudhubs.prophetutils.ProphetUtilsFacade;
+import edu.baylor.ecs.cloudhubs.prophetutils.filemanager.FileManager;
 
 public class Main {
     public static void main(String[] args) {
--- src/test/java/ProphetUtilsTest.java
+++ src/test/java/ProphetUtilsTest.java
@@ -2,9 +2,13 @@ import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.BoundedContext;
 
 import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.SystemContext;
 import edu.baylor.ecs.cloudhubs.prophetutils.ProphetUtilsFacade;
+import edu.baylor.ecs.cloudhubs.prophetutils.filemanager.FileManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
+
+import java.io.File;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -35,6 +39,21 @@ public class ProphetUtilsTest {
     public void boundedContextGen() {
         BoundedContext boundedContext = ProphetUtilsFacade.getBoundedContext(rootPath, microServicePaths);
         assertNotNull(boundedContext.getSystemName());
+    }
+
+    @Test
+    @DisplayName("generating graph")
+    public void generatingGraph(){
+        String template = FileManager.readHtmlTemplates("src/main/resources/header.html");
+        template += FileManager.readHtmlTemplates("src/main/resources/footer.html");
+        assertNotNull(template);
+    }
+
+    @Test
+    @DisplayName("generating template")
+    public void generateTemplate(){
+        ProphetUtilsFacade.createHtmlTemplate(rootPath, microServicePaths);
+        assertNotNull(1);
     }
 
 }
