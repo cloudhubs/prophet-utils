759bd3f9618ccb447ce0175cf3aecb41fcd2a661
David Kozak <david.kozak@oracle.com>
2022-12-19 11:09:12 +0100

    first NI specific classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import com.google.gson.Gson;
 import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.BoundedContext;
+import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.SystemContext;
 import edu.baylor.ecs.cloudhubs.prophetutils.ProphetUtilsFacade;
 import edu.baylor.ecs.cloudhubs.prophetutils.filemanager.FileManager;
+import edu.baylor.ecs.cloudhubs.prophetutils.nativeimage.MicroserviceInfo;
+import edu.baylor.ecs.prophet.bounded.context.api.impl.BoundedContextApiImpl;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
 
+        String baseDir = "/Users/dkozak/Projects/prophet/jars";
+        List<MicroserviceInfo> microservices = Arrays.asList(
+                new MicroserviceInfo(baseDir, "edu.baylor.ecs.cms", "cms"),
+                new MicroserviceInfo(baseDir, "edu.baylor.ems", "ems"),
+                new MicroserviceInfo(baseDir, "baylor.csi", "qms"));
+
+        SystemContext ctx = ProphetUtilsFacade.getSystemContextViaNativeImage(microservices);
+        Gson gson = new Gson();
+        FileManager.writeToFile(ctx, "ni-prophet.json");
+        System.out.println(gson.toJson(ctx));
+        BoundedContext boundedContext = new BoundedContextApiImpl().getBoundedContext(ctx, false);
+        FileManager.writeToFile(boundedContext, "ni-bounded-context.json");
     }
 }
---
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetutils/nativeimage/NativeImageRunner.java
@@ -0,0 +1,76 @@
+package edu.baylor.ecs.cloudhubs.prophetutils.nativeimage;
+
+import com.google.gson.Gson;
+import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.Module;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NativeImageRunner {
+
+    private static final String NI_BASE = "/Users/dkozak/Projects/graal/sdk/mxbuild/darwin-amd64/GRAALVM_37AE0F4241_JAVA17/graalvm-37ae0f4241-java17-23.0.0-dev/Contents/Home";
+
+    private static final String NI_CMD = NI_BASE + "/bin/native-image";
+
+    private final String microservicePath;
+    private final String classpath;
+    private final String outputJson;
+
+    private final MicroserviceInfo info;
+
+    public NativeImageRunner(MicroserviceInfo info) {
+        this.info = info;
+        this.microservicePath = info.getBaseDir() + File.separator + info.getMicroserviceName();
+        this.classpath = microservicePath + "/BOOT-INF/classes" + ":" + microservicePath + "/BOOT-INF/lib/*";
+        this.outputJson = "out.json";
+    }
+
+    public Module runProphetPlugin() {
+        executeNativeImage();
+        return parseOutputFile();
+    }
+
+    private Module parseOutputFile() {
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(outputJson)) {
+            return gson.fromJson(reader, Module.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void executeNativeImage() {
+        List<String> cmd = prepareCommand();
+        System.out.println(cmd);
+        try {
+            Process process = new ProcessBuilder()
+                    .command(cmd)
+                    .inheritIO()
+                    .start();
+            int res = process.waitFor();
+            if (res != 0) {
+                System.err.println("Failed to execute command.");
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NotNull
+    private List<String> prepareCommand() {
+        List<String> cmd = new ArrayList<>();
+        cmd.add(NI_CMD);
+        cmd.add("-cp");
+        cmd.add(classpath);
+        cmd.add("-H:+ProphetPlugin");
+        cmd.add("-H:ProphetModuleName=" + this.info.getMicroserviceName());
+        cmd.add("-H:ProphetBasePackage=" + this.info.getBasePackage());
+        cmd.add("-H:ProphetOutputFile=" + this.outputJson);
+        cmd.add("dummy-main");
+        return cmd;
+    }
+}
