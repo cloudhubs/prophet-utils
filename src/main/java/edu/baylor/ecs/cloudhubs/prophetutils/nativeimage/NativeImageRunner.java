280610f39c423471440399328ae4aa0cf96b4e85
RichardHutcheson <richardhutch80@gmail.com>
2023-03-20 00:10:59 -0500

    add line to native image command to take out inlinebeforeanalysis

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetutils/nativeimage/NativeImageRunner.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetutils/nativeimage/NativeImageRunner.java
@@ -62,13 +62,14 @@ public class NativeImageRunner {
     private List<String> prepareCommand() {
         List<String> cmd = new ArrayList<>();
         cmd.add(niCommand);
-        cmd.add("-cp");
-        cmd.add(classpath);
         cmd.add("-H:+ProphetPlugin");
         cmd.add("-H:+ProphetRest");
+        cmd.add("-H:-InlineBeforeAnalysis");
         cmd.add("-H:ProphetModuleName=" + this.info.getMicroserviceName());
         cmd.add("-H:ProphetBasePackage=" + this.info.getBasePackage());
-        cmd.add("-H:ProphetOutputFile=" + this.outputJson);
+        cmd.add("-H:ProphetOutputFile=" + this.outputJson);        
+        cmd.add("-cp");
+        cmd.add(classpath);
         cmd.add(this.info.getMicroserviceName());
         return cmd;
     }
